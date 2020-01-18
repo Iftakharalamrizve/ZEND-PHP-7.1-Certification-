cd215e818bade718eb6ffc80acad82ee7bfef042
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-01-19 03:11:35 +0600

    zend study

--- php_basic/php_tag.php
+++ php_basic/php_tag.php
@@ -1,8 +1,15 @@
-<?php 
-$expression=true;
-if($expression==true): 
-?>
-Hello Expression is ture ;
-<?php else: ?>
-Hello Expression is false ;
-<?php endif; ?>
+//php tags related problem 
+<?="Hello Bangladesh"?> //php short open tag 
+<?php echo "Hello " ?> //available now  
+ <%, %> //asp tag deprecated from 
+ <%= %> //asp short open tag but remove from 
+ <script language="php"> // script tag remove from php 
+
+
+ //php 
+ <?php/*hello*/ echo "Hello"?> //show error must be have  nedd space means whitespace after php tag then run this line
+ <?php /*hello*/ echo "Hello"?> // no problem  it's ok
+ <?php
+ /*hello*/ echo "Hello"?>  // no problem it's ok
+
+
