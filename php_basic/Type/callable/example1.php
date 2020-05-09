b869c6bf499af236c4c1e4f3da01e54f34ca4584
Iftakharalamrizve <iftakharalam32@gmail.com>
2020-05-10 00:32:28 +0600

    callback function example

--- php_basic/Type/callable/example1.php
+++ php_basic/Type/callable/example1.php
@@ -2,7 +2,23 @@
 
 class MyFunction{
 
-    private function testFunction(callable  $callBack)
+    //callback method 
+    private function sayHi(){
+        echo "Hi........";
+    }
+
+    //callable function 
+    public function testAccess(){
+        $this->privateCallbackTest([$this,'sayHi']);
+    }
+
+    public  function testFunction(callable  $callBack)
+    {
+        $callBack();
+    }
+
+
+    private function privateCallbackTest(callable $callBack)
     {
         $callBack();
     }
@@ -19,5 +35,9 @@ function sayHi()
 
 $objectInstance = new MyFunction();
 
+$objectInstance->testAccess();
 //if public method this is work just fine 
 $objectInstance->testFunction('sayHi');
+
+
+//if private and protected method use callback then follow this rule  in class 
